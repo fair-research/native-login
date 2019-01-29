e4fd0eb0e5c050676d3d4e152169c54c577ba77b
Nickolaus D. Saint <nick@globus.org>
2019-01-29 07:48:55 -0800

    Added checks to token loading, fixed local server client login

--- native_login/client.py
+++ native_login/client.py
@@ -1,6 +1,9 @@
+import time
 from globus_sdk import NativeAppAuthClient
 
-from native_login.code_handler import InputCodeHandler, LocalServerCodeHandler
+from native_login.code_handler import InputCodeHandler
+from native_login.local_server import LocalServerCodeHandler
+from native_login.exc import LoadError, TokensExpired, ScopesMismatch
 
 
 class NativeClient(NativeAppAuthClient):
@@ -9,30 +12,31 @@ class NativeClient(NativeAppAuthClient):
                  requested_scopes=None, *args, **kwargs):
         super(NativeClient, self).__init__(*args, **kwargs)
         self.token_handler = token_handler
-        self.code_handler = local_server_code_handler
         self.requested_scopes = requested_scopes
-        self.app_name = kwargs.get('app_name')
+        self.app_name = kwargs.get('app_name') or 'My App'
+        self.local_server = (local_server_code_handler or
+                             LocalServerCodeHandler())
+        self.local_server.set_app_name(self.app_name)
 
     def login(self, no_local_server=False, no_browser=False,
               requested_scopes=(), refresh_tokens=None,
               prefill_named_grant=None, additional_params=None):
-        code_handler = (InputCodeHandler() if no_local_server
-                        else LocalServerCodeHandler())
-        name = self.app_name or 'Anonymous Client'
-        grant_name = prefill_named_grant or 'Grant for {}'.format(name)
+        grant_name = prefill_named_grant or '{} Login'.format(self.app_name)
+        code_handler = (InputCodeHandler()
+                        if no_local_server else self.local_server)
 
-        self.oauth2_start_flow(refresh_tokens=refresh_tokens,
-                               prefill_named_grant=grant_name,
-                               redirect_uri=code_handler.redirect_uri)
-        auth_url = self.oauth2_get_authorize_url(
-            additional_params=additional_params
-        )
-        try:
-            auth_code = self.code_handler.authenticate(url=auth_url,
-                                                       no_browser=no_browser)
-        except Exception:
-            auth_code = InputCodeHandler().authenticate(url=auth_url,
-                                                        no_browser=no_browser)
+        with code_handler.start():
+            self.oauth2_start_flow(
+                requested_scopes=requested_scopes,
+                refresh_tokens=refresh_tokens,
+                prefill_named_grant=grant_name,
+                redirect_uri=code_handler.get_redirect_uri()
+            )
+            auth_url = self.oauth2_get_authorize_url(
+                additional_params=additional_params
+            )
+            auth_code = code_handler.authenticate(url=auth_url,
+                                                  no_browser=no_browser)
         token_response = self.oauth2_exchange_code_for_tokens(auth_code)
         self.save_tokens(token_response)
         return token_response
@@ -42,10 +46,29 @@ class NativeClient(NativeAppAuthClient):
             serialized_tokens = self.token_handler.serialize(tokens)
             return self.token_handler.write(serialized_tokens)
 
-    def load_tokens(self):
+    def load_tokens(self, requested_scopes=None):
         if self.token_handler is not None:
             serialized_tokens = self.token_handler.read()
-            return self.token_handler.deserialize(serialized_tokens)
+            tokens = self.token_handler.deserialize(serialized_tokens)
+
+            if not tokens:
+                raise LoadError('No Tokens loaded')
+
+            if requested_scopes is not None:
+                scopes = [tset['scope'].split() for tset in tokens.values()]
+                flat_list = [item for sublist in scopes for item in sublist]
+                if set(flat_list) != set(requested_scopes):
+                    raise ScopesMismatch('Requested Scopes do not match loaded'
+                                         ' Scopes for Globus Auth.')
+
+            expired = [
+                time.time() >= t["expires_at_seconds"]
+                for t in tokens.values()
+            ]
+            if any(expired):
+                raise TokensExpired()
+
+            return tokens
 
     def revoke_tokens(self):
         tokens = self.load_tokens()
