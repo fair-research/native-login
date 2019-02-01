be12bb649e8ca56cde20141dea8cf0254971f9ec
Nickolaus D. Saint <nick@globus.org>
2019-02-01 13:36:27 -0800

    Added some useful hints to basic usage example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -4,10 +4,15 @@ a local server for logging in users.
 """
 from native_login import NativeClient
 
-# Register an app for a client_id at https://developers.globus.org
+# Register a Native App for a client_id at https://developers.globus.org
 client = NativeClient(client_id='7414f0b4-7d05-4bb6-bb00-076fa3f17cf5')
 
 # Automatically saves tokens in ~/.globus-native-apps.cfg
-tokens = client.login(requested_scopes=['openid'])
+tokens = client.login(
+    # Request any scopes you want to use here.
+    requested_scopes=['openid'],
+    # In order to use a local server on your app, you MUST include:
+    # "http://localhost" under "Redirect URIs"
+    no_local_server=False)
 
 print(tokens)
