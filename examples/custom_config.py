83b37576c10a586d41eb9bf5f670ad658922604e
Nickolaus D. Saint <nick@globus.org>
2019-02-05 13:24:48 -0800

    Updated custom config example, is now runnable

--- examples/custom_config.py
+++ examples/custom_config.py
@@ -3,33 +3,37 @@ Example for a custom config.
 
 At some point, your client will probably need to have it's own config mechanism
 for your app-specific config values. To use a custom config, all you need to
-do is provide the read/write/clear hooks.
+do is provide the read_tokens/write_tokens/clear_tokens functions in an object.
 """
-from native_login import NativeClient, TokenStorage
-from myconfig import config
+import json
+import os
+from native_login import NativeClient
 
 
-class MyTokenStorage(TokenStorage):
+class MyTokenStorage(object):
+    FILENAME = 'my_tokens.json'
 
     def write_tokens(self, tokens):
-        config.save(tokens, section='tokens')
+        with open(self.FILENAME, 'w+') as fh:
+            json.dump(tokens.by_resource_server, fh, indent=2)
 
     def read_tokens(self):
-        config.load(section='tokens')
+        with open(self.FILENAME) as fh:
+            return json.load(fh)
 
     def clear_tokens(self):
-        config.remove(section='tokens')
+        os.remove(self.FILENAME)
 
 
 # Using your custom Token Handler:
-app = NativeClient(client_id='',
-                   token_handler=MyTokenStorage())
+app = NativeClient(client_id='7414f0b4-7d05-4bb6-bb00-076fa3f17cf5',
+                   token_storage=MyTokenStorage())
 
-# Calls read() then write()
+# Calls read_tokens() then write_tokens()
 app.login()
 
-# Calls read()
-tokens = app.load_tokens()
+# Calls read_tokens()
+app.load_tokens()
 
-# Calls clear()
+# Calls clear_tokens()
 app.revoke_tokens()
