fa2df0def10b59e55f645c813e9a42986fdf5ccd
Nickolaus D. Saint <nick@globus.org>
2019-02-11 12:07:51 -0800

    Added README as long_description in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ version_ns = {}
 with open(os.path.join('fair_research_login', 'version.py')) as f:
     exec(f.read(), version_ns)
 
+with open('README.md') as f:
+    long_description = f.read()
+
 install_requires = []
 with open('requirements.txt') as reqs:
     for line in reqs.readlines():
@@ -15,8 +18,9 @@ with open('requirements.txt') as reqs:
         install_requires.append(req)
 
 setup(
-    name="fair-research-login",
-    description="A generalized library for storing native auth tokens",
+    name='fair-research-login',
+    description='A generalized library for storing native auth tokens',
+    long_description=long_description,
     url='https://github.com/fair-research/native-login',
     maintainer='Fair Research',
     maintainer_email='',
@@ -30,13 +34,13 @@ setup(
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        "Operating System :: POSIX",
-        "Operating System :: MacOS :: MacOS X",
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ]
 )
